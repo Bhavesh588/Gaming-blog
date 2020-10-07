d7c873a1d5ce2a13cecd6bf8c0f8baa2209438b0
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2020-10-07 17:42:23 +0530

    Updating

--- src/component/Hero/Hero.js
+++ src/component/Hero/Hero.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 import './Hero.scss';
 
@@ -9,17 +11,27 @@ function Hero() {
                 <ol class="carousel-indicators carousel-indicators-numbers">
                     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">1</li>
                     <li data-target="#carouselExampleIndicators" data-slide-to="1">2</li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2">3</li>
                 </ol>
                 <div class="carousel-inner">
-                    <div class="carousel-item homepage__imgbox active">
-                        <img class="d-block homepage__image" src={require('F:\\Websites\\Gaming Blog\\gaming-blog\\src\\assets\\pexels-fox-3829227.jpg')} alt="First slide" />
+                    <div class="carousel-item active">
+                        <div className="hero__content">
+                            <h1>Gamers World!</h1>
+                            <p className="hero__des">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <button className="hero__read">Read More <ArrowForwardIosIcon className="arrowbtn__1" /><ArrowForwardIosIcon className="arrowbtn__2" /></button>
+                        </div>
+                        <div className="homepage__imgbox">
+                            <img class="d-block homepage__image" src={require('F:\\Websites\\Gaming Blog\\gaming-blog\\src\\assets\\pexels-fox-3829227.jpg')} alt="First slide" />
+                        </div>
                     </div>
                     <div class="carousel-item homepage__imgbox">
-                        <img class="d-block homepage__image" src={require('F:\\Websites\\Gaming Blog\\gaming-blog\\src\\assets\\pexels-lalesh-aldarwish-194511.jpg')} alt="Second slide" />
-                    </div>
-                    <div class="carousel-item homepage__imgbox">
-                        <img class="d-block homepage__image" src={require('F:\\Websites\\Gaming Blog\\gaming-blog\\src\\assets\\pexels-fox-3829227.jpg')} alt="Third slide" />
+                        <div className="hero__content">
+                            <h1>Gamers News!</h1>
+                            <p className="hero__des">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <button className="hero__read">Read More <ArrowForwardIosIcon className="arrowbtn__1" /><ArrowForwardIosIcon className="arrowbtn__2" /></button>
+                        </div>
+                        <div className="homepage__imgbox">
+                            <img class="d-block homepage__image" src={require('F:\\Websites\\Gaming Blog\\gaming-blog\\src\\assets\\pexels-lalesh-aldarwish-194511.jpg')} alt="Second slide" />
+                        </div>
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">

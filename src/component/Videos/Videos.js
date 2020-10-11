fe668399d0feba7ab79aaec80279037b4dc7c960
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2020-10-11 18:16:03 +0530

    Animations

--- src/component/Videos/Videos.js
+++ src/component/Videos/Videos.js
@@ -11,7 +11,7 @@ function Videos() {
     const Lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
     return (
-        <div className="videos">
+        <div className="videos" data-aos="fade-right">
             <div className="container">
                 <div className="row videos__row py-3">
                     <div className="col-md-4">
